f276d79a650d62b33f80f83dda2e4e948c5b3ac8
Mariana Montes <mariana.montes@kuleuven.be>
2025-06-25 16:15:39 +0200

    add some examples

---
+++ examples/example_script.R
@@ -0,0 +1,15 @@
+# First heading ####
+library(tidyr)
+
+## Second heading ====
+add_one <- function(x) {
+  x + 1
+}
+
+add_one(2)
+
+# Another first heading ----
+# TODO: assign to a variable?
+add_one(4) |> add_one()
+
+print(d)
